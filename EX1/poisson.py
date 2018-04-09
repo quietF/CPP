8222ea63283a1e9a7ef89b3574eb13d897290db0
quietF <a.cathey.c@gmail.com>
2018-04-09 18:53:12 +0200

    Matrix solution for finite difference scheme (Dirichlet B.C.)

--- EX1/poisson.py
+++ EX1/poisson.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Grid division
-N  = 8
+N  = 32
 a  = 0.
 b  = 2.*np.pi
 L  = b - a
@@ -12,30 +12,32 @@ x  = np.linspace(a, b, N)
 def rho_0(x):
   return 2.*np.sin(x) + x*np.cos(x)
 rho = rho_0(x)
+h  = x[1] - x[0]
+h2 = h*h
 
 # initial conditions for potential 
-phi                 = np.zeros(np.size(x))
-phi[0]              = 0.
-phi[np.size(phi)-1] = 2.*np.pi
+alpha = 0.
+beta  = 2.*np.pi
 
-#plt.plot(x, rho, '-r', x, phi, '-b')
-#plt.show()
+Rho               = np.zeros(np.size(x))
+Rho[:]            = rho[:]
+Rho[0]            = rho[0] + alpha / h2
+Rho[np.size(x)-1] = rho[np.size(x)-1] + beta / h2
 
 # analytical solution for potential
 def phi_true(x):
   return x*np.cos(x)
 
-a = np.zeros((N,N));     b = np.zeros((N,N))
-np.fill_diagonal(a, -1); np.fill_diagonal(b, -1)
-a = np.roll(a, 1);       b = np.roll(b, -1)
+# matrix A_ij phi(x_i) = rho(x_i)
+a = np.zeros((N,N));         b = np.zeros((N,N))
+np.fill_diagonal(a, -1./h2); np.fill_diagonal(b, -1./h2)
+a = np.roll(a, 1);           b = np.roll(b, -1)
+A_ij = a + b
+np.fill_diagonal(A_ij, 2./h2)
+#print(np.linalg.eigvals(T))
 
-T = a + b
-np.fill_diagonal(T, 2)
-print(np.linalg.eigvals(T))
+A_ij_inv   = np.linalg.inv(A_ij)
+newPhi = np.dot(A_ij_inv, Rho)
 
-
-#dphi                   = np.zeros(np.size(x))
-#dphi[1:np.size(phi)-1] = (phi[2:np.size(phi)] - 2.*phi[1:np.size(phi)-1] + phi[0:np.size(phi)-2]) / (x[1] - x[0])
-#phi                    = phi + dphi
-
-#print(phi)
+plt.plot(x, newPhi, '-r', x, phi_true(x), '--g', x, rho, '-b')
+plt.show()
