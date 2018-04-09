d11a6d26399f1a4e26c1b599b4a58d4dba21bef0
quietF <a.cathey.c@gmail.com>
2018-04-09 17:04:45 +0200

    Notes

--- EX1/poisson.py
+++ EX1/poisson.py
@@ -21,13 +21,17 @@ phi[np.size(phi)-1] = 2.*np.pi
 #plt.plot(x, rho, '-r', x, phi, '-b')
 #plt.show()
 
+# analytical solution for potential
+def phi_true(x):
+  return x*np.cos(x)
+
 a = np.zeros((N,N));     b = np.zeros((N,N))
 np.fill_diagonal(a, -1); np.fill_diagonal(b, -1)
 a = np.roll(a, 1);       b = np.roll(b, -1)
 
 T = a + b
 np.fill_diagonal(T, 2)
-print(np.linalg.eig(T))
+print(np.linalg.eigvals(T))
 
 
 #dphi                   = np.zeros(np.size(x))
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CPP
 Computational Plasma Physics Course Repo
 
+http://www-m16.ma.tum.de/foswiki/pub/M16/Allgemeines/CompPlasmaPhys18/CompPlasmaPhys17.pdf
+
 CPP/EX1:
 	1D Poisson solver with finite differences.
 	
